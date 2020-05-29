8162933dedcef2331d4aa1345b3a57dbc1382601
CoffeeHaus <neumann6994@gmail.com>
2020-05-29 13:34:10 -0500

    Update AddToAutoRun.ps1

--- Powershell/AddToAutoRun.ps1
+++ Powershell/AddToAutoRun.ps1
@@ -1,1 +1,6 @@
-New-ItemProperty -path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -name "Anything" -value "PathtoEXE" -propertytype "string"
+#Commands to add exe to registry
+
+$PathtoEXE = "C:\Users\user\Documents\tool.exe"
+$DecoyName = "Windows Windowing Service"
+
+New-ItemProperty -path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -name "Anything" -value $PathtoEXE -propertytype "string"
